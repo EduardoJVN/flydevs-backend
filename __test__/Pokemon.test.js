9eff5716fbd2e77de50ebcde391adc143e10752f
theBalckWolfTech <ejvn30@gmail.com>
2021-02-09 21:59:53 -0300

    version 1.0.1 fix test in pokemon name

--- __test__/Pokemon.test.js
+++ __test__/Pokemon.test.js
@@ -17,18 +17,27 @@ it("searchPokemons should bring an array with the pokemons according to the para
         "ditto",
         "bulbasaur",
         "charizard",
-        "+"
+        "+",
+        ""
     ]
     for(let index in parameters){
         const response = await request(app).get(`/pokemon/${parameters[index]}`).set('Accept','application/json').set('Content-type','application/json')
         expect(response).toBeDefined()
         expect(response).not.toBeNull()
         expect(response.header['content-type']).toMatch(/json/)
-        expect(response.status).toBe(200)
         if(index == 4){
             expect(response.body.length).toEqual(0)
+            expect(response.status).toBe(200)
         }else{
-            expect(response.body.length).toBeGreaterThanOrEqual(1)
+            if(index == 5){
+                expect(response.status).toBe(404)
+                expect(response.body.message).toBeDefined()
+                expect(response.body.message).not.toBeNull()
+                expect(response.body.message).toBe('Pagina no encontrada')
+            }else{
+                expect(response.status).toBe(200)
+                expect(response.body.length).toBeGreaterThanOrEqual(1)
+            }
         }
     }    
 })
